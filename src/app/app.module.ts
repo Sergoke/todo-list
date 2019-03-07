e0faa48a7cdd6396ae51400891ded1e1d78a35f1
Sergey <seroga0313@gmail.com>
2019-03-07 10:13:34 +0200

    added checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { ButtonsModule} from './buttons/buttons.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './list/list.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListComponent,
-    TodoInputComponent
+    TodoInputComponent,
+    CheckboxComponent
   ],
   imports: [
     BrowserModule,
